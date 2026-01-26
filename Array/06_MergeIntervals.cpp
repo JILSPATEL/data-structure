cc7eb912b2fc34e259ec5ceab4e779119b621f1d
JILSPATEL <jils18062003@gmail.com>
2026-01-26 18:29:25 +0530

    added merge intevals code

---
+++ Array/06_MergeIntervals.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    vector<vector<int>> merge(vector<vector<int>>& intervals) {
+        int n=intervals.size();
+        if(n==0) return {};
+
+        sort(intervals.begin(),intervals.end());
+        vector<vector<int>>res;
+        res.push_back(intervals[0]);
+        for(int i=1;i<n;i++){
+            vector<int>&prev_end=res.back();
+            if(prev_end[1]>=intervals[i][0]){
+                prev_end[1]=max(prev_end[1],intervals[i][1]);
+            }
+            else{
+                res.push_back(intervals[i]);
+            }
+        }
+        return res;
+    }
+};
+
+      
