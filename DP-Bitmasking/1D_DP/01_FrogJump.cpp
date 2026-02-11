364cd98d2f974a3b682e12d1f1e5003fec3a4246
JILSPATEL <jils18062003@gmail.com>
2026-02-11 12:03:40 +0530

    added approach-2

--- DP-Bitmasking/1D_DP/01_FrogJump.cpp
+++ DP-Bitmasking/1D_DP/01_FrogJump.cpp
@@ -98,3 +98,26 @@ class Solution {
         return prev1;
     }
 };
+
+/*
+=================================================
+Approach-2
+=================================================
+*/
+class Solution {
+  public:
+    int minCost(vector<int>& height) {
+        int n=height.size();
+        vector<int> dp(n);
+        dp[0]=0;
+        for(int i=1;i<n;i++){
+            int jumpTwo=INT_MAX;
+            int jumpOne=dp[i-1]+abs(height[i]-height[i-1]);
+            if(i>1){
+                jumpTwo=dp[i-2]+abs(height[i]-height[i-2]);
+            }
+            dp[i]=min(jumpOne,jumpTwo);
+        }
+        return dp[n-1];
+    }
+};
